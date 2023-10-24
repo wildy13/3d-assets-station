c426be6abb05edb53724d6520cc0c40b3e0c9794
yozzd <yozzdharma@gmail.com>
2023-10-24 10:58:31 +0700

    init

---
+++ api/index.js
@@ -0,0 +1,49 @@
+import 'dotenv/config';
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import jwt from '@fastify/jwt';
+import { connect } from 'mongoose';
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.register(cors);
+fastify.register(jwt, { secret: process.env.SESSION_KEY, sign: { expiresIn: '8h' } });
+
+fastify.addHook('onRequest', async (req, res) => {
+  try {
+    if (req.headers.authorization) {
+      await req.jwtVerify();
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+const connector = async () => {
+  try {
+    await connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serializeFunctions: true,
+    });
+
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    fastify.log.error(error);
+    process.exit(1);
+  }
+};
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: process.env.PORT, host: process.env.HOST });
+    await connector();
+  } catch (error) {
+    fastify.log.error(error);
+    process.exit(1);
+  }
+};
+
+start();
