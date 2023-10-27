b9a35c1865ef2574fc9c41b9b7dc2b4eb8d45d7f
yozzd <yozzdharma@gmail.com>
2023-10-27 15:01:28 +0700

    refactor(api/user): add isAdmin middleware to routes

--- api/user/index.js
+++ api/user/index.js
@@ -1,8 +1,10 @@
 import { getAll, create } from './controller.js';
 
+import { isAdmin } from '../auth/service.js';
+
 export default ((fastify, opts, done) => {
-  fastify.get('/', getAll);
-  fastify.post('/create', create);
+  fastify.get('/', { preHandler: [isAdmin()] }, getAll);
+  fastify.post('/create', { preHandler: [isAdmin()] }, create);
 
   done();
 });
